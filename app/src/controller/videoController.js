c2bfd476d763d97149efd3252b29df8d11675240
조원재 <hadnjswp@gmail.com>
2023-04-05 11:39:55 +0900

    add portfilo

--- app/src/controller/videoController.js
+++ app/src/controller/videoController.js
@@ -17,67 +17,65 @@ MongoClient.connect("mongodb+srv://kdKim:6r7r6e5!KD@cluster0.mo9rckf.mongodb.net
         db = client.db('test');
     });
 
-// fs 모듈 사용하여 video 파일 
-const videos = async function (req, res) {
-console.log("들어옴")
-// 요청 헤더에서 range 가져오기
-const { id } = parseInt(req.params.id);
-console.log(id);
-await db.collection("post").findOne({id_:id});
-const {range} = req.headers; 
-
-// 요청 받은 range 없으면 상태코드400 보내기
-if (range) {
-    res.status(400).send("Requires Range header");
-};
-// 파라미터로 전달된 ID 값 파싱
-
-// MongoDB에서 ID 값에 해당하는 동영상 데이터 조회
-const collection = db.collection('post')
-collection.findOne({
-    _id : id
-    }, function(err, result){
-    // 에러 발생
-    if (err) { 
-        console.log('동영상 데이터 조회 실패');
-        return res.status(500).send('Internal Server Error');
-    }
-    // 조회된 결과가 없을 경우
-    if (!result) {
-        return res.status(404).send('Not Found');
-    }
-    // 조회된 결과가 있을 경우 요청된 동영상 파일의 경로 생성
-    
-    // 파일의 상태를 동기적으로 가져오기
-    const stat = fs.statSync(videoPath)
-    // 파일 크기 
-    const videoSize = stat.size;
-    // 전송 할 데이터 제한
-    const CHUNK_SIZE = 10 ** 6; // 1MB
-    // start값 숫자로 변환
-    const start = Number(range.replace(/\D/g, "")); // 숫자가 아닌 것 지우는 정규식
-    // end값 숫자로 변환 and end값 없을 경우 "파일크기-1" 값 할당
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-
-    // 전송할 데이터 크기
-    const contentLength = (end - start) + 1;
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+// 동영상 파일 경로 생성 함수
+const getVideoPath = (id) => {
+    return `videos/${id}.mp4`;
+  };
+  
+  // 동영상 스트리밍을 처리하는 핸들러 함수
+  const videos = async function (req, res) {
+    try {
+      const id = parseInt(req.params.id);
+      const { range } = req.headers;
+  
+      // id가 숫자가 아닐 경우 400 오류 반환
+      if (isNaN(id)) {
+        return res.status(400).send("Invalid ID");
+      }
+  
+      // 동영상 파일 경로 생성
+      const videoPath = getVideoPath(id);
+      const stat = fs.statSync(videoPath);
+      const fileSize = stat.size;
+      const CHUNK_SIZE = 10 ** 6; // 1MB
+  
+      // range 헤더에서 시작 지점(start) 추출
+      const start = Number(range.replace(/\D/g, ""));
+  
+      // range 헤더에서 끝 지점(end) 추출하거나 파일 크기 - 1 지점으로 설정
+      const end = Math.min(start + CHUNK_SIZE, fileSize - 1);
+  
+      // 요청한 범위가 파일 크기를 넘어설 경우 416 오류 반환
+      if (start >= fileSize || end >= fileSize) {
+        return res.status(416).send("Requested Range Not Satisfiable");
+      }
+  
+      // 응답 헤더 설정
+      const contentLength = end - start + 1;
+      const headers = {
+        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
         "Accept-Ranges": "bytes",
         "Content-Length": contentLength,
         "Content-Type": "video/mp4",
-    };
-
-    //상태코드 206 반환 / 206 : 클라이언트에서 복수의 스트림을 분할 다운로드를 하고자 범위 헤더를 전송 성공시 사용
-    res.writeHead(206, headers);
-
-    // 비디오 읽기 스트림 생성
-    const videoStream = fs.createReadStream(videoPath, { start, end });
-
-    // 클라이언트로 스트리밍 반환
-    videoStream.pipe(res);
+      };
+      res.writeHead(206, headers);
+  
+      // 동영상 파일 읽기 스트림 생성
+      const videoStream = fs.createReadStream(videoPath, { start, end });
+  
+      // 파일 읽기 스트림에서 에러 발생 시 500 오류 반환
+      videoStream.on("error", (err) => {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+      });
+  
+      // 파일 읽기 스트림과 응답 스트림을 연결하여 동영상 스트리밍 반환
+      videoStream.pipe(res);
+    } catch (err) {
+      // 예기치 않은 에러 발생 시 500 오류 반환
+      console.error(err);
+      res.status(500).send("Internal Server Error");
     }
-);
-}
+  };
 
 module.exports = videos;
